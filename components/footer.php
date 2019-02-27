6fa9d68ca799f147b97611f10158ae2195facb40
Carolia Alessandra <carolina@maxmeio.com>
2019-02-27 15:14:35 -0300

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,21 @@
         <footer>
+            
+                <div class="container">
+                    <div class="rodape">
+                        <div class="contato">
+                            <p><i class="fa fa-mobile" aria-hidden="true"></i>+ 55 84 9 9962-8000</p>
+                            <p><i class="fa fa-envelope" aria-hidden="true"></i>vaniellyff@hotmail.com.br</p>
+                        </div>
+                        <div class="redes">
+                            <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </div>
+                    
+                    </div>
+                </div>
+            
+        	
 			
         </footer>
 
